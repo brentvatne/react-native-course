572479973ef24c0f6083b5e935e70c7c9a0c92e4
Brent Vatne <brentvatne@gmail.com>
2018-03-19 17:00:38 -0400

    Final

--- lecture/6-navigation/App.js
+++ lecture/6-navigation/App.js
@@ -1,43 +1,43 @@
 // import Example from './examples/0-Switch';
-import Example from './examples/1-Stack';
-export default Example;
+// import Example from './examples/1-Stack';
+// export default Example;
 
-// import React from 'react';
-// import { createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+import { createSwitchNavigator } from 'react-navigation';
 
-// import AddContactScreen from './screens/AddContactScreen';
-// import ContactListScreen from './screens/ContactListScreen';
-// import contacts from './contacts';
+import AddContactScreen from './screens/AddContactScreen';
+import ContactListScreen from './screens/ContactListScreen';
+import contacts from './contacts';
 
-// const Navigation = createSwitchNavigator(
-//   {
-//     ContactList: ContactListScreen,
-//     AddContact: AddContactScreen,
-//   },
-//   {
-//     initialRouteName: 'ContactList',
-//   }
-// );
+const Navigation = createSwitchNavigator(
+  {
+    ContactList: ContactListScreen,
+    AddContact: AddContactScreen,
+  },
+  {
+    initialRouteName: 'ContactList',
+  }
+);
 
-// export default class App extends React.Component {
-//   state = {
-//     contacts,
-//   };
+export default class App extends React.Component {
+  state = {
+    contacts,
+  };
 
-//   addContact = newContact => {
-//     this.setState(prevState => ({
-//       contacts: [...prevState.contacts, newContact],
-//     }));
-//   };
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
 
-//   render() {
-//     return (
-//       <Navigation
-//         screenProps={{
-//           contacts: this.state.contacts,
-//           addContact: this.addContact,
-//         }}
-//       />
-//     );
-//   }
-// }
+  render() {
+    return (
+      <Navigation
+        screenProps={{
+          contacts: this.state.contacts,
+          addContact: this.addContact,
+        }}
+      />
+    );
+  }
+}
